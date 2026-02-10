0f072994f0a308b48d9507ac3a2282e475a5b1f0
Farooq Bin Munir <farooqbinmunir@gmail.com>
2026-02-10 19:58:10 +0500

    New module created stock-filter

---
+++ templates/sections/stock-filter.php
@@ -0,0 +1,5 @@
+<select id="stockFilter" name="stockFilter">
+    <option value="all-stock">All Stock</option>
+    <option value="near-to-end">Near to End</option>
+    <option value="out-of-stock">Out of Stock</option>
+</select>

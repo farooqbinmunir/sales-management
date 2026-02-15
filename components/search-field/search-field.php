d8889560831d979c909b208d05972fa37d9cfa4c
Farooq Bin Munir <farooqbinmunir@gmail.com>
2026-02-16 02:30:51 +0500

    Component search field udpated to get values from context array

--- components/search-field/search-field.php
+++ components/search-field/search-field.php
@@ -1,1 +1,1 @@
-<input type="search" name="" id="search-product" placeholder="Search here..." data-search-column="<?php echo $search_column ?? 'pname'; ?>" data-row-to-skip="<?php echo $row_to_skip ?? ''; ?>" />
+<input type="search" name="" id="search-product" placeholder="Search here..." data-search-column="<?php echo isset($data['search_column']) ? $data['search_column'] : 'pname'; ?>" data-row-to-skip="<?php echo isset($data['row_to_skip']) ? $data['row_to_skip'] : ''; ?>" />

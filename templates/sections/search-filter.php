384c98478077353f831d481a1065989a1f2b3019
Farooq Bin Munir <farooqbinmunir@gmail.com>
2026-02-16 00:20:00 +0500

    Deleted unused files(Frontend)

--- templates/sections/search-filter.php
+++
@@ -1,9 +0,0 @@
-<div id="searchFilter">
-    <div>Search by:</div>
-    <select id="searchFilterType">
-        <option value="product_name">Product Name</option>
-        <option value="product_vendor">Product Vendor</option>
-        <option value="product_manufacturer">Product Manufacturer</option>
-    </select>
-    <input type="search" name="" id="search-product" placeholder="Search Product Name">
-</div>
--- templates/sections/stock-filter.php
+++
@@ -1,5 +0,0 @@
-<select id="stockFilter" name="stockFilter">
-    <option value="all-stock">All Stock</option>
-    <option value="near-to-end">Near to End</option>
-    <option value="out-of-stock">Out of Stock</option>
-</select>

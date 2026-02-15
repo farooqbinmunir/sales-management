f5fca158c87a6d7be88000620be5b19c15392b54
Farooq Bin Munir <farooqbinmunir@gmail.com>
2026-02-16 02:32:43 +0500

    Component Search Filter updated, removed hard coded options to dynamically get options and output customized filter

--- components/search-filter/search-filter.php
+++ components/search-filter/search-filter.php
@@ -1,9 +1,15 @@
+<?php 
+$options = $data['options'] ?? $data;
+$row_to_skip = $data['row_to_skip'] ?? '';
+?>
 <div id="searchFilter">
     <div>Search by:</div>
     <select id="searchFilterType">
-        <option value="pname">Product Name</option>
-        <option value="pvendor">Product Vendor</option>
-        <option value="pmanufacturer">Product Manufacturer</option>
+        <?php 
+        foreach($options as $key => $value){
+            echo "<option value='{$key}'>{$value}</option>";
+        }
+        ?>
     </select>
-    <?php include_component('search-field'); ?>
+    <?php include_component('search-field', ['search_column' => key($options) ?? 'pname', 'row_to_skip' => $row_to_skip]); ?>
 </div>

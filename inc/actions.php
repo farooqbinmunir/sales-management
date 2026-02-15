c6c14ff77c9b644ce9f30051513b38a67c653599
Farooq Bin Munir <farooqbinmunir@gmail.com>
2026-02-16 00:36:46 +0500

    Removed action for frontend enqueue + clean up

--- inc/actions.php
+++ inc/actions.php
@@ -1,36 +1,19 @@
 <?php
 
-
-
-
-
 	// Including functions.php file to use it's functions here
-
 	require_once(FBM_PLUGIN_DIR . 'inc/functions.php');
 
-	
-
 	// Add Menu to admin menu bar
-
 	add_action('admin_menu', 'fbm_admin_menu_callback');
 
-
-
 	// Enqueue scripts & styles for backend
-
 	add_action('admin_enqueue_scripts', 'fbm_backend_enqueues');
 
-
-
-	// Enqueue scripts and styles for frontend
-
-	add_action('wp_enqueue_scripts', 'fbm_frontend_enqueues');
-
-
-
 	// Do stuff on wordpress init, like creating a post type
-
 	add_action('init', 'fbm_init_callback');
 
-
+	// AJAX action for deleting old entries
 	add_action('admin_init', 'delete_entries_older_than_two_years_once_per_day');
+
+	// AJAX action for verifying user credentials in the authentication popup
+	add_action('wp_ajax_fbm_verify_user', 'fbm_verify_user');
